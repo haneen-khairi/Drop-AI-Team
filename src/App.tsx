b8a9e209960ec28ed92255e8d917cfd8649b5b22
John1316 <johnmaher179@gmail.com>
2023-07-24 21:00:26 +0300

    add details page

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const App: React.FC = () => {
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/social-search" element={<SocialSearch />} />
           <Route path="/social-word" element={<SocialWord />} />
-          <Route path="/social-page/:id" element={<SocialPage />} />
+          <Route path="/social-page/:name" element={<SocialPage />} />
           <Route path="/more-ai" element={<SeeMore />} />
           <Route path="/more-tictok" element={<SeeMore />} />
           <Route path="/more-deals" element={<SeeMore />} />
--- src/pages/SocialPage.tsx
+++ src/pages/SocialPage.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { Interest, NLPChart, Post } from 'utils/types';
-import { Button, Container, Modal, Row as SuiteRow } from 'rsuite';
-import { useLocation } from 'react-router-dom';
+import { Button, Container, Modal, Table  } from 'rsuite';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Col, Row } from 'react-bootstrap';
 import FacebookInterestCard from '../components/FacebookInterestCard';
 import { FiFileText } from '@react-icons/all-files/fi/FiFileText';
@@ -13,36 +13,74 @@ import TableCnt from '../components/TableCnt';
 import { getNLP, getPosts, wordStatistics } from 'utils/api';
 import { useQuery } from '@tanstack/react-query';
 import SideList from '../components/SideList';
+import axios from 'axios';
 // import downloadIcon from '../assets/img/download-01.svg';
-
+const { Column, HeaderCell, Cell } = Table;
+interface Details {
+    
+        pages: [],
+        comments: [],
+        mobile: string,
+        website: string,
+        Email: string
+    
+}
 const SocialPage: React.FC = () => {
+    const navigate = useNavigate()
     const location = useLocation();
+    const params = useParams();
+
+    const [name, setName] = useState('')
+    const [details, setDetails] = useState<Details>()
+    const [pages, setPages] = useState([])
+    const [comments, setComments] = useState([])
     const [query,] = useState<string>(location.state?.socialSearchData?.searchValue || '');
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    // const [open, setOpen] = React.useState(false);
+    // const handleOpen = () => setOpen(true);
+    // const handleClose = () => setOpen(false);
 
     const { data: stats, isLoading: statsLoading } = useQuery<Interest, Error>(['Word-Interest', query], () => {
         return wordStatistics(query);
     },);
 
-    const { data: posts, isLoading: postsLoading } = useQuery<Post[], Error>(['Word-Posts', query], () => {
-        return getPosts(query);
-    },);
+    // const { data: posts, isLoading: postsLoading } = useQuery<Post[], Error>(['Word-Posts', query], () => {
+    //     return getPosts(query);
+    // },);
 
-    const { data: nlpChart, isLoading: nlpLoading, refetch } = useQuery<NLPChart, Error>(['NLP-Chart', query], () => {
-        return getNLP(query);
-    }, {
-        enabled: false
-    });
+    // const { data: nlpChart, isLoading: nlpLoading, refetch } = useQuery<NLPChart, Error>(['NLP-Chart', query], () => {
+    //     return getNLP(query);
+    // }, {
+    //     enabled: false
+    // });
 
-    const handleShow = () => {
-        refetch();
-        handleOpen();
+    // const handleShow = () => {
+    //     refetch();
+    //     handleOpen();
+    // }
+    async function getDetails(name:string){
+        await axios.get(`https://dropshipping-app-ingsl.ondigitalocean.app/facebook/page_details/?page_name=${name}`).then((data) => {
+            console.log('data ==>',data.data)
+            setDetails(data.data)
+            setPages(data.data.pages)
+            setComments(data.data.comments)
+        }).catch((error) => {
+            console.log('error in fetch' , error)
+        })
     }
-
-
+    useEffect(() => {
+        if (params && params.name !== undefined) {
+            const page_name: string = params.name;
+            setName(page_name)
+            console.log('name ==>',page_name)
+            // Now you can safely use the 'name' variable which will be of type 'string'
+            // Your code here...
+            getDetails(page_name)
+        } 
+    
+      
+    }, [])
+    
     return (
         <>
             <Header className="search-header" />
@@ -50,9 +88,13 @@ const SocialPage: React.FC = () => {
                 <Row className='page-boundaries'>
                     <Col style={{ display: "flex", flexDirection: "column", minHeight: "500px" }}>
                         <h3 className='title'>
-                            Searched Page: {String(query)}
+                            Searched Page: {name}
                         </h3>
-
+                        <div className="">
+                            <p>Email: {details?.Email === '' ? 'No email found' : details?.Email}</p>
+                            <p>Website: {details?.website === '' ? 'No website found' : details?.website}</p>
+                            <p>Mobile: {details?.mobile === '' ? 'No website found' : details?.mobile}</p>
+                        </div>
                         <Row className='statistics-row'>
                             <Row>
                                 <p className='sub-title'>
@@ -62,7 +104,7 @@ const SocialPage: React.FC = () => {
                             <Row>
                                 <Col>
                                     <FacebookInterestCard
-                                        content={statsLoading ? "Loading..." : String(stats?.page_number || 0)}
+                                        content={statsLoading ? "Loading..." : String(pages.length || 0)}
                                         text="Page Number"
                                         Icon={FiFile}
                                         color="#EDEDF7"
@@ -71,8 +113,8 @@ const SocialPage: React.FC = () => {
                                 </Col>
                                 <Col>
                                     <FacebookInterestCard
-                                        content={statsLoading ? "Loading..." : String(stats?.posts_number || 0)}
-                                        text="Posts Number"
+                                        content={statsLoading ? "Loading..." : String(comments.length || 0)}
+                                        text="Comments Number"
                                         Icon={FiFileText}
                                         color="#F7EDED"
                                         detailList={[String(query), 'post']}
@@ -81,7 +123,7 @@ const SocialPage: React.FC = () => {
                                 <Col>
                                     <FacebookInterestCard
                                         content={statsLoading ? "Loading..." : String(stats?.users_number || 0)}
-                                        text="Users Number"
+                                        text="Last comment in user"
                                         Icon={FiUsers}
                                         color="#ECF3E5"
                                         detailList={[String(query), 'user']}
@@ -120,31 +162,37 @@ const SocialPage: React.FC = () => {
                         </Col>
 
                     </Row>
-                    <SuiteRow className='table-row'>
-                        <TableCnt isLoading={postsLoading} data={(posts ? posts : [])} />
-                    </SuiteRow>
-                    <Row >
-                        <Col className='d-flex justify-content-center'>
-                            <Modal overflow={true} open={open} onClose={handleClose}>
-                                <Modal.Header>
-                                    <Modal.Title>NLP</Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body className='modalbod'>
-                                    {
-                                        nlpLoading ?
-                                            <p>
-                                                Loading...
-                                            </p>
-                                            :
-                                            <img className='NLP-Chart' src={`${nlpChart?.link}`} alt={`${query} image not fount`} />
-                                    }
-                                </Modal.Body>
-                            </Modal>
-                            <Button onClick={handleShow} style={{ paddingRight: "120px", paddingLeft: "120px" }} size="lg" className='filter-btn' color='violet' appearance="primary">
-                                Show NLP
-                            </Button>
-                        </Col>
-                    </Row>
+                    <div className='table-row'>
+                        {/* <TableCnt isLoading={postsLoading} data={(posts ? posts : [])} /> */}
+                        <Table
+                            // loading={isLoading}
+                            bordered
+                            className='table'
+                            height={400}
+                            headerHeight={54}
+                            data={pages}
+                            
+                        >
+                            <Column verticalAlign="middle" flexGrow={2} minWidth={300}>
+                                <HeaderCell>Page content</HeaderCell>
+                                <Cell dataKey="Post_Content" />
+                            </Column>
+                            <Column verticalAlign="middle" width={200} >
+                                <HeaderCell>Page name</HeaderCell>
+                                <Cell dataKey="page" />
+                            </Column>
+                            <Column verticalAlign="middle" width={200} >
+                                <HeaderCell>name</HeaderCell>
+                                <Cell dataKey="name" />
+                            </Column>
+                            <Column verticalAlign="middle" width={200} >
+                                <HeaderCell>Comments number</HeaderCell>
+                                <Cell dataKey="Comments.length" />
+                            </Column>
+
+                        </Table>
+                    </div>
+                    
 
                 </Row>
             </Container>
